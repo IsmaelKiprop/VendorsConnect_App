5d6a8c0abc9b1e63b98b7cad7e9081bc9ebba4d9
Ismael Kiprop <133222922+IsmaelKiprop@users.noreply.github.com>
2023-07-16 05:57:36 +0300

    Updated

--- vc/app/urls.py
+++ vc/app/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'app'
+
 urlpatterns = [
     path("", views.home),
+     path('product/<barcode>/', views.product_details_view, name='product-details'),
+         
 ]
+
--- vc/app/views.py
+++ vc/app/views.py
@@ -1,5 +1,36 @@
 from django.shortcuts import render
+from .openfoodfacts import get_product_details
+from .openfoodfacts import fetch_dairy_products
 
 # Create your views here.
 def home(request):
     return render(request,"app/home.html")
+
+def product_details_view(request, barcode):
+    product_data = get_product_details(barcode)
+    # Process and display the product data as needed
+
+def home(request):
+    # Fetch dairy products data from the Open Food Facts API
+    dairy_products = fetch_dairy_products()  # Replace this with your own logic
+
+    context = {
+        'dairy_products': dairy_products,
+    }
+    return render(request, 'app/home.html', context)
+
+
+def product_details_view(request, barcode):
+    product_data = get_product_details(barcode)
+    # Process and validate product_data as needed
+    if product_data:
+        # Render a template or create a response
+        context = {
+            'product_data': product_data,
+        }
+        return render(request, 'app/product_details.html', context)
+    else:
+        # Return an appropriate response when product data is not found
+        return render(request, 'app/product_not_found.html')
+
+
